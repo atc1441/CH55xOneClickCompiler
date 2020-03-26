a9a998a41c830458ac5573b5d2de8ebb04a130ef
atc1441 <1102694+atc1441@users.noreply.github.com>
2020-03-26 11:55:29 +0100

    Update debug.h

--- debug.h
+++ debug.h
@@ -1,6 +1,3 @@
-﻿/* 调试 */
-/* 提供printf子程序和延时函数 */
-
 #pragma once
 
 #include <stdint.h>
@@ -13,23 +10,24 @@
 #define  UART1_BUAD    9600
 #endif
 
-void	CfgFsys( );                        //CH554时钟选择和配置
+void	CfgFsys( );
 
-void	mDelayuS( uint16_t n );              // 以uS为单位延时
-void	mDelaymS( uint16_t n );              // 以mS为单位延时
+void	mDelayuS( uint16_t n );
+void	mDelaymS( uint16_t n );
 
 
-void  CH554UART0Alter();                 //CH554串口0引脚映射到P0.2/P0.3
-void	mInitSTDIO( );                      //T1作为波特率发生器
-uint8_t CH554UART0RcvByte( );              //CH554 UART0查询方式接收一个字节
-void  CH554UART0SendByte(uint8_t SendDat); //CH554UART0发送一个字节
+void  CH554UART0Alter();
+void	mInitSTDIO( );
+uint8_t CH554UART0RcvByte( );
+void  CH554UART0SendByte(uint8_t SendDat);
 
-void	UART1Setup( );                     //
-uint8_t CH554UART1RcvByte( );              //CH554 UART1查询方式接收一个字节
-void  CH554UART1SendByte(uint8_t SendDat); //CH554UART1发送一个字节
+void	UART1Setup( );
+uint8_t CH554UART1RcvByte( );
+void  CH554UART1SendByte(uint8_t SendDat);
 
-void putchar(char data);
-char getchar();
 
-void CH554WDTModeSelect(uint8_t mode);     //CH554看门狗模式设置
-void CH554WDTFeed(uint8_t tim);            //CH554看门狗喂狗
+void CH554WDTModeSelect(uint8_t mode);
+void CH554WDTFeed(uint8_t tim);
+
+typedef void(* __data FunctionReference)();
+extern FunctionReference runBootloader;
